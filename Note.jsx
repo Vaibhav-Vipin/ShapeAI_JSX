6f3fe545ba71523bb8047ddf2911c65c3e8e65b3
Vaibhav-Vipin <39304129+Vaibhav-Vipin@users.noreply.github.com>
2021-06-06 13:20:55 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+function Note() {
+  return (
+    <div className="note">
+      <h1> Javascript and React.js</h1>
+      <p>
+        React (also known as React.js or ReactJS) is an open-source front-end
+        JavaScript library[3] for building user interfaces or UI components. It
+        is maintained by Facebook and a community of individual developers and
+        companies.
+      </p>
+    </div>
+  );
+}
+export default Note;
